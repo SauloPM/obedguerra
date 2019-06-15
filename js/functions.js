54999966e6f7e51a4c46677c5a7de7ae91b2d409
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-06-15 19:37:07 +0100

    [ Index ] » Animations added

--- js/functions.js
+++ js/functions.js
@@ -10,34 +10,14 @@ $(document).ready(function() {
 
     // Navbar
     fixNavbar();
-    highlightNavbar();
 
     // Top Button
     showTopButton();
-    
-    // Slider
-    // slider();
 
 });
 
-// Header Slider
-// function slider() {
-//     setTimeout(function() {
-        
-//         var ruta = $("#header").css('background-image')
-//         var i = ruta[ruta.length - 7]
-
-//         i = i == 4 ? 1 : ++i
-
-//         $("#header").css({"background" : "url(img/bg/header" + i + ".jpg) no-repeat center", "background-size" : "cover"});
-
-//         slider();
-//     }, 5000);
-// }
-
 $(document).scroll(function() {
     fixNavbar();
-    highlightNavbar();
     showTopButton();
 });
 
@@ -64,42 +44,6 @@ function fixNavbar() {
     }
     else {
         $("nav").css("box-shadow", "");
-    }
-}
-
-// Current Item Highlighting
-function highlightNavbar() {
-
-    console.log($("#info").position().top);
-
-    // Information
-    if ($(this).scrollTop() < $("#info").position().top - 150) {
-        $("nav .item").removeClass("active");
-        $("nav .item:nth-child(1)").addClass("active");
-    }
-
-    // Career
-    if ($(this).scrollTop() >= $("#career").position().top -150) {
-        $("nav .item").removeClass("active");
-        $("nav .item:nth-child(2)").addClass("active");
-    }
-
-    // Skills
-    if ($(this).scrollTop() >= $("#skills").position().top - 150) {
-        $("nav .item").removeClass("active");
-        $("nav .item:nth-child(3)").addClass("active");
-    }
-
-    // Training
-    if ($(this).scrollTop() >= $("#training").position().top - 150) {
-        $("nav .item").removeClass("active");
-        $("nav .item:nth-child(4)").addClass("active");
-    }
-
-    // Contact
-    if ($(this).scrollTop() >= $("#contact").position().top - 300) {
-        $("nav .item").removeClass("active");
-        $("nav .item:nth-child(5)").addClass("active");
     }
 }
 
