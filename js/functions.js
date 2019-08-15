4a91bfac18340463a5136b7b1257818a5f1a19c2
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-08-15 02:04:24 +0100

    [ JS ] » Código refactorizado

--- js/functions.js
+++ js/functions.js
@@ -1,69 +1,66 @@
-$(document).ready(function() {
+$(window).on("load", function() {
 
     // WOW
     new WOW({
         mobile: false
-    }).init(); 
+    }).init();
 
-    // Spinner
+    // Smooth Scrolling Links
+    $(".smooth-scrolling").click(function (event) {
+        event.preventDefault();
+        var target = $(this).attr("href");
+        $("html, body").stop().animate({ scrollTop: $(target).offset().top }, 1500, 'easeInOutExpo');
+    });
+
+    // ─────────────── //
+    //     SPINNER     //
+    // ─────────────── //
+
     hideSpinner();
 
-    // Navbar
-    fixNavbar();
+    // Spinner hidding
+    function hideSpinner () {
+        $("#preloader .spinner").fadeOut(500, function () {
+            $("#preloader").delay(500).fadeOut(500);
+            setTimeout(function () { $("body").css("overflow", "visible"); }, 500);
+        });
+    }
 
-    // Top Button
+    // ────────────────── //
+    //     TOP BUTTON     //
+    // ────────────────── //
+
     showTopButton();
 
-});
+    $(document).scroll(function() {
+        showTopButton();
+    });
 
-$(document).scroll(function() {
+    // Top button revealing
+    function showTopButton () {
+        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150)
+            $(".top-button").css("opacity", "1");
+        else
+            $(".top-button").css("opacity", "0");
+    }
+
+    // ────────────── //
+    //     NAVBAR     //
+    // ────────────── //
+
     fixNavbar();
-    showTopButton();
-});
 
-// ─────────────── //
-//     SPINNER     //
-// ─────────────── //
-
-// Spinner hidding
-function hideSpinner () {
-    $("#preloader .bar").fadeOut(500, function () {
-        $("#preloader").delay(500).fadeOut(500);
-        setTimeout(function () { $("body").css("overflow", "visible"); }, 500);
+    $(document).scroll(function() {
+        fixNavbar();
     });
-}
 
-// ────────────── //
-//     NAVBAR     //
-// ────────────── //
-
-// Stycky Navigation Bar
-function fixNavbar() {
-    if ($(document).scrollTop() > 0) {
-        $("nav").css("box-shadow", "0 0 5px black");
+    // Stycky Navigation Bar
+    function fixNavbar() {
+        if ($(document).scrollTop() > 0) {
+            $("nav").css("box-shadow", "0 0 5px black");
+        }
+        else {
+            $("nav").css("box-shadow", "");
+        }
     }
-    else {
-        $("nav").css("box-shadow", "");
-    }
-}
-
-// ────────────────── //
-//     TOP BUTTON     //
-// ────────────────── //
-
-// Smooth Scrolling Links
-$(".smooth-scrolling").click(function (event) {
-    event.preventDefault();
-    var target = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(target).offset().top }, 800, function () {
-        window.location.hash = target;
-    });
 });
-
-// Top button revealing
-function showTopButton () {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150)
-        $(".top-button").css("opacity", "1");
-    else
-        $(".top-button").css("opacity", "0");
-}
