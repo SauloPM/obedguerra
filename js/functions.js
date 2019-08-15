855bba51c8f832ee3952b442f1578d11c6a4328e
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-08-15 23:52:11 +0100

    [ Navbar ] » Navbar animations completed

--- js/functions.js
+++ js/functions.js
@@ -49,7 +49,7 @@ $(window).on("load", function() {
     // ────────────── //
 
     $(document).on("click", "#navbar .abrir", function() {
-        $("#navbar .contenido").css("left", "0");
+        
     });
 
     $(document).on("click", "#navbar .cerrar", function() {
@@ -60,39 +60,66 @@ $(window).on("load", function() {
     //     BURGER BUTTON     //
     // ───────────────────── //
 
+    // Abrir
     $(document).on("click", "#burger-button.closed", function() {
 
-        $(this).removeClass("closed").addClass("opened");
+        $(this).removeClass("closed");
 
+        // Ocultamos la barra intermedia
         $("#burger-button .barra:nth-child(2)").css("transform", "scaleX(0)");
 
+        // Desplazamos las dos barras de los extremos al centro
         setTimeout(() => {
             $("#burger-button .barra:nth-child(1)").css({"top": "8px", "opacity": "0"});
             $("#burger-button .barra:nth-child(3)").css({"top": "8px", "opacity": "0"});
+        }, 250);
 
-            setTimeout(() => {
-                $("#burger-button .barra:nth-child(1)").css({"transform": "rotate(45deg)", "opacity": "1"});
-                $("#burger-button .barra:nth-child(3)").css({"transform": "rotate(-45deg)", "opacity": "1"});
-            }, 500);
+        // Rotamos estas barras para darle apariencia de botón de cierre
+        setTimeout(() => {
+            $("#burger-button .barra:nth-child(1)").css({"transform": "rotate( 45deg)", "opacity": "1"});
+            $("#burger-button .barra:nth-child(3)").css({"transform": "rotate(-45deg)", "opacity": "1"});
         }, 500);
+
+        // Mostramos el navbar
+        setTimeout(() => { $("#navbar").css({"opacity": "1", "transform": "scaleX(1)"}) }, 750);
+
+        // Mostramos los ítems del navbar
+        setTimeout(() => { $("#navbar .item:nth-child(1)").css("opacity", "1") }, 1000);
+        setTimeout(() => { $("#navbar .item:nth-child(2)").css("opacity", "1") }, 1250);
+        setTimeout(() => { $("#navbar .item:nth-child(3)").css("opacity", "1") }, 1500);
+
+        setTimeout(() => { $(this).addClass("opened") }, 1500);
     });
 
+    // Cerrar
     $(document).on("click", "#burger-button.opened", function() {
 
-        $(this).removeClass("opened").addClass("closed");
+        $(this).removeClass("opened");
 
-        $("#burger-button .barra:nth-child(1)").css({"transform": "rotate(0)", "opacity": "0"});
-        $("#burger-button .barra:nth-child(3)").css({"transform": "rotate(0)", "opacity": "0"});
+        // Ocultamos los ítems del navbar
+        $("#navbar .item:nth-child(1)").css("opacity", "");
+        setTimeout(() => { $("#navbar .item:nth-child(2)").css("opacity", "") }, 250);
+        setTimeout(() => { $("#navbar .item:nth-child(3)").css("opacity", "") }, 500);
 
+        // Mostramos el navbar
+        setTimeout(() => { $("#navbar").css({"opacity": "", "transform": "scaleX(0)"}) }, 750);
+
+        // Rotamos las barras de los extremos para que vuelvan a tener una orientación horizontal
+        setTimeout(() => {
+            $("#burger-button .barra:nth-child(1)").css({"transform": "rotate(0)", "opacity": "0"});
+            $("#burger-button .barra:nth-child(3)").css({"transform": "rotate(0)", "opacity": "0"});
+        }, 1000);
+
+        // Desplazamos estas barras del centro a los extremos
         setTimeout(() => {
             $("#burger-button .barra:nth-child(1)").css({"top": "", "opacity": "1"});
             $("#burger-button .barra:nth-child(3)").css({"top": "", "opacity": "1"});
+        }, 1250);
 
-            setTimeout(() => {
-                $("#burger-button .barra:nth-child(2)").css("transform", "scaleX(1)");
-            }, 500);
-        }, 500);
+        // Mostramos la barra intermedia
+        setTimeout(() => { $("#burger-button .barra:nth-child(2)").css("transform", "scaleX(1)") }, 1500);
 
+        setTimeout(() => { $(this).addClass("closed") }, 1500);
     });
 
     // ────────────── //
